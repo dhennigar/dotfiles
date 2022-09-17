1c391cb7001402bb792de13613fe956ddabb6e62
dhennigar <dhennigar@protonmail.com>
2022-09-16 17:19:33 -0700

    updates

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,8 @@
 -- Basics
 vim.opt.autoindent = true
 vim.opt.cursorline = true
+vim.g.mapleader = ','
+vim.g.maplocalleader = ';'
 
 -- Plugins
 local Plug = vim.fn['plug#']
@@ -19,3 +21,11 @@ vim.call('plug#end')
 -- 	style = 'darker'
 -- }
 require('onedark').load()
+
+-- Nvim-R
+vim.cmd([[
+let R_auto_start = 1
+let R_objbr_auto_start = 1
+let R_objbr_place = 'console,above'
+let R_assign_map = '<M-->'
+]])
