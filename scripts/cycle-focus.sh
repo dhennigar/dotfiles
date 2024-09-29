eb385861226b5381bf4d4895483440ca9d63f00b
Daniel Hennigar <danrhennigar@gmail.com>
2024-09-29 01:22:57 -0700

    working script

---
+++ scripts/cycle-focus.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# cycle-focus.sh
+
+PROG=$( basename "$0" )
+wm="i3"
+msg="i3-msg"
+[[ "$SWAYSOCK" ]] && {
+    wm="sway"
+    msg="swaymsg"
+}
+
+# TODO: Implement a reverse cycle. Woudl require proper indexing, I think.
+reverse=""
+
+TEMP=$( getopt --options rh --longoptions reverse,help -- "$@" ) || exit 1
+eval set -- "$TEMP"
+
+for i in "$@"; do
+    case "$i" in
+        -h|--help)
+            echo "Usage: $PROG OPTIONS target [runstring]"
+            echo
+            echo "Cycle focus through the windows on the current workspace."
+            echo
+            echo "OPTIONS"
+            echo "-r|--reverse             cycle in reverse (NOT YET IMPLEMENTED)"
+            exit 0
+            ;;
+        -r|--reverse)
+            reverse="set"
+            ;;
+    esac
+done
+
+# Get the current workspace
+workspace=$( $msg -t get_workspaces |
+                  jq -r '.[] | select(.focused==true) | .name' |
+                  awk -F':' '{print $1}' )
+
+# Get the currently focused window
+focused=$( $msg -t get_tree | jq '.. | select(.type?) | select(.focused==true) | .id' )
+
+# Get the list of all window ids
+# window_ids=$( $msg -t get_tree | jq -r '.. | select(.type?=="con") | .id')
+
+# Get the list of window ids on the current workspace
+window_ids=$( $msg -t get_tree | jq -r --arg ws $workspace '.. | select(.type?=="workspace") | select(.name==$ws) | recurse(.nodes[]) | select(.name != null and .type?=="con") | .id' ) 
+
+# If windows exist, find the focused one in the list, then focus the next one
+if [ -n "$window_ids" ]; then
+
+    found_focused=false
+    for window in $window_ids; do
+	if $found_focused; then
+	    swaymsg "[con_id=$window] focus"
+	    exit 0
+	fi
+	if [ "$window" = "$focused" ]; then
+	    found_focused=true
+	fi
+    done
+
+    # If we reach the end of the list, focus the first window
+    first_window=$(echo "$window_ids" | head -n 1)
+    swaymsg "[con_id=$first_window]" focus	
+
+fi
